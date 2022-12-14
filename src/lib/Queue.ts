ddbbde38edcb82386983a48f4f18f3afad4e7742
Jefferson <jefferson.costa@rarolabs.com>
2022-12-14 11:18:56 -0300

    ajuste nas configruações do redis

--- src/lib/Queue.ts
+++ src/lib/Queue.ts
@@ -1,31 +1,40 @@
-import Queue from 'bull';
-import redisConfig from '../config/redis';
+import Queue from "bull";
+import redisConfig from "../config/redis";
 
-import * as jobs from '../jobs/MailRegister';
+import * as jobs from "../jobs/MailRegister";
 // const audioQueue = new Queue('audio transcoding', ); // Specify Redis connection using object
+const host:string  = process.env.REDIS_HOST || '127.0.0.1';
+const port: string  = process.env.REDIS_PORT || '6379';
+const password: string  = process.env.REDIS_PASSWORD || '';
 
-const queues = Object.values(jobs).map(job => ({
-  bull: Queue(job.key , { redis: { port: 6379, host: '127.0.0.1', password: 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81' } }),
+const queues = Object.values(jobs).map((job) => ({
+  bull: Queue(job.key, {
+    redis: {
+      port: parseInt(port),
+      host: host,
+      password: password,
+    },
+  }),
   name: job.key,
   handle: job.handle,
   options: job.options,
-}))
+}));
 
 export default {
   queues,
-  add(name:string, data:any) {
-    const queue = this.queues.find(queue => queue.name === name);
-    
+  add(name: string, data: any) {
+    const queue = this.queues.find((queue) => queue.name === name);
+
     return queue!.bull.add(data, queue!.options);
   },
   process() {
-    return this.queues.forEach(queue => {
+    return this.queues.forEach((queue) => {
       queue.bull.process(queue.handle);
 
-      queue.bull.on('failed', (job, err) => {
-        console.log('Job failed', queue.name, job.data);
+      queue.bull.on("failed", (job, err) => {
+        console.log("Job failed", queue.name, job.data);
         console.log(err);
       });
-    })
-  }
+    });
+  },
 };
